e9278e32f3e1e39e244462743dbfcfe23c26f227
abeylann <alicia.navarrete@deliveroo.co.uk>
2016-07-08 15:37:32 +0100

    Added several lists to one single place

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ApplicationRecord
 	belongs_to :places
+	has_many :places
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,24 @@ List.delete_all
   beach = Place.create(name: 'Beach')
   forest = Place.create(name: 'Forest')
   skiing = Place.create(name: 'Skiing')
+  hackathon = Place.create(name: 'Hackathon')
 
 
-basic = '{ Shoes, Makeup}'
-summer = '{ Hat, Sunglasses}'
-winter = '{ Scarf, Gloves }'
-forestt = '{ Boots, Coat, Hat}'
+basic = '{ Shampoo, Conditioner, Toothbrush, Toothpaste, Deodorant, Towel, Hairbrush,
+			Phone Charger, Earphones,  ID, Underwear, Socks, PJs, Slippers}'
+summer = '{ Hat, Sunglasses, Flip Flops, Bathing Suit, Sunscreen, Lip Balm,
+			Hand Sanitizer, Body Lotion}'
+winter = '{ Scarf, Gloves, Beanie, Coat, Cardigan, Boots, Blanket }'
+forestt = '{ Boots, Coat, Raincoat, Wellie Boots, Sunscreen, Hand Sanitizer, 
+			Sleeping bag, Blanket}'
+hack = '{ Sleeping bag, Eye mask, Toothbrush, Toothpaste, Dry Shampoo, Hairbrush,
+				Laptop Charger, Phone Charger, Headphones, Hand Sanitizer, Deodorant}'
 
-  list1 = List.create(items: basic, place_id: city.id)
+  list1 = List.create(items: basic, place_id: skiing.id)
   list2 = List.create(items: summer, place_id: beach.id)
   list3 = List.create(items: winter, place_id: skiing.id)
   list4 = List.create(items: forestt, place_id: forest.id)
+  list5 = List.create(items: hack, place_id: hackathon.id)
+  list6 = List.create(items: basic, place_id: city.id)
+  list7 = List.create(items: basic, place_id: beach.id)
 
